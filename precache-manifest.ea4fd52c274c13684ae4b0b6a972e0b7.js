92cfdd468c89b1193aa3ecd47b42dc1c9c71f359
Amin Ali <mithun@localhost.localdomain>
2020-01-26 14:23:56 +0600

    weather app push

---
+++ precache-manifest.ea4fd52c274c13684ae4b0b6a972e0b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "12e5c2fe1ffca3537fc0d460f3947ae4",
+    "url": "/static/media/weather.12e5c2fe.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "64cc71c7d92a7920e052",
+    "url": "/static/js/main.64cc71c7.chunk.js"
+  },
+  {
+    "revision": "c1c2c5f49e7a75b45c29",
+    "url": "/static/js/2.c1c2c5f4.chunk.js"
+  },
+  {
+    "revision": "64cc71c7d92a7920e052",
+    "url": "/static/css/main.c35d8cf1.chunk.css"
+  },
+  {
+    "revision": "b34d03222a1e6c13f16a46d800d2b585",
+    "url": "/index.html"
+  }
+];
